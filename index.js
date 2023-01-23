f12a0946061a9825eb709e3d642215e02e7756a3
Ibraheem <iejibode@gmail.com>
2023-01-23 08:30:09 +0100

    assignment

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function Rectangle(height, width, height) {
-  this.height = height;
-  this.width = width;
-  this.height = height;
-  this.calcArea = function () {
-    return this.height * this.width;
-  };
-  this.calcPerimeter = function () {
-    return (this.height + this.width) * 2;
-  };
+class rectangle {
+  constructor(height, width, length) {
+    this.height = height;
+    this.width = width;
+    this.height = height;
+    this.calcArea = function () {
+      return this.height * this.width;
+    };
+    this.calcPerimeter = function () {
+      return (this.height + this.width) * 2;
+    };
+  }
 }
-let rectang = new Rectangle(10, 15);
+let rectang = new rectangle(10, 15);
 let area = rectang.calcArea();
 let perimeter = rectang.calcPerimeter();
 console.log(area, perimeter);
